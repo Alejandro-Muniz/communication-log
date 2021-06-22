a5690e65a9528046d9d1cc4365091e76142c9675
Alejandro Muniz <alejandro.muniz@yahoo.com>
2021-06-22 12:27:22 -0400

    Started communicationlog.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace communication_log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
